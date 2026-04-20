ae6f1bc501d15bad8820bbe122e017dc530c31f1
Christopher <107579643+chrisco-jr@users.noreply.github.com>
2026-04-20 12:13:55 -0700

    Add some helper functions

--- main.cpp
+++ main.cpp
@@ -4,9 +4,20 @@
 #include "Sweep_History.h"
 #include "Sweep_Profile.h"
 #include <iostream>
+#include <string.h>
 #include <limits>
+#include <stdlib.h>
 
 using namespace std;
+
+// Function Prototypes
+char** processStrToArr(const char *input, int *count);
+bool enter_device_names(char *input);
+void task_exit();
+
+const char *DEVICES[] = {"dev1", "dev2", "dev3", "dev4"};
+	int DEVICE_COUNT = sizeof(DEVICES) / sizeof(DEVICES[0]);
+
 int main()
 {
 	int state = 0;
@@ -42,49 +53,74 @@ int main()
 		    case 1:
 		    {
 			    cout << "\nSTATUS selected";
+				string devices;
+				bool allValid = false;
+				while (!allValid)
+				{
+					cout << "\nEnter device name(s) to see status of, separated by a comma\n";
+					getline(cin >> ws, devices);
+					if(enter_device_names((char*)devices.c_str()))
+					{	
+						cout << "All devices valid";
+						allValid = true;
+						break;
+					}
+					else
+					{
+						cout << "Please try again.\n";
+					}
+				}
 			    state = 0;
+				task_exit();
 			    break;
 		    }
 		    case 2:
 		    {
 			    cout << "\nCONFIGURE PROFILE selected";
 			    state = 0;
+				task_exit();
 			    break;
 		    }
 		    case 3:
 		    {
 			    cout << "\nLOAD PROFILE selected";
 			    state = 0;
+				task_exit();
 			    break;
 		    }
 		    case 4:
 		    {
 			    cout << "\nDELETE PROFILE selected";
 			    state = 0;
+				task_exit();
 			    break;
 		    }
 		    case 5:
 		    {
 			    cout << "\nPLAY PROFILE selected";
 			    state = 0;
+				task_exit();
 			    break;
 		    }
 		    case 6:
 		    {
 			    cout << "\nSTOP PROFILE selected";
 			    state = 0;
+				task_exit();
 			    break;
 		    }
 		    case 7:
 		    {
 			    cout << "\nACCESS LOG selected";
 			    state = 0;
+				task_exit();
 			    break;
 		    }
 		    case 8:
 		    {
 			    cout << "\nDELETE LOG selected";
 			    state = 0;
+				task_exit();
 			    break;
 		    }
 		    
@@ -93,8 +129,89 @@ int main()
 		        state = 0;
 		        break;
 		    }
-		    
-		}
 
+		}
 	}
+}
+
+void task_exit()
+{
+	cout << "\n\nTask Complete, returning to Main Menu. Hit Ctrl + C to exit the program\n";
+}
+
+bool enter_device_names(char *input)
+{
+	int count = 0;
+	char** deviceNames = processStrToArr(input, &count);
+
+	if (deviceNames == NULL && count == 0)
+	{
+		cout << "No devices entered. Valid devices:\n";
+		for (int i = 0; i < DEVICE_COUNT; i++)
+		{
+			printf("- %s\n", DEVICES[i]);
+		}
+		return false;
+	}
+
+	bool allValid = true;
+
+	for (int i = 0; i < count; i++)
+	{
+		bool isValid = false;
+		for (int j = 0; j < DEVICE_COUNT; j++)
+		{
+			if (strcmp(deviceNames[i], DEVICES[j]) == 0) {
+                isValid = true;
+                break;
+            }
+		}
+		if (!isValid)
+		{
+			allValid = false;
+			printf("Error: '%s' is not a valid device.\n", deviceNames[i]);
+			cout << "Valid devices:\n";
+			for (int i = 0; i < DEVICE_COUNT; i++)
+			{
+				printf("- %s\n", DEVICES[i]);
+			}
+		}
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		free(deviceNames[i]);
+	}
+	free(deviceNames);
+	return allValid;
+}
+
+char** processStrToArr(const char *input, int *count)
+{
+	extern const char* DEVICES[];
+    extern int DEVICE_COUNT;
+	
+	// processes a list of strings seprated by commas, returns as an array
+	char **result = NULL;
+	char *tmpInput = strdup(input);
+	char *token = strtok(tmpInput, ",");
+	*count = 0;
+
+	while (token != NULL)
+	{
+		while (*token == ' ') token++;
+		char **tmpResult = (char**)realloc(result, sizeof(char*) * (*count + 1));
+		if (tmpResult == NULL)
+		{
+			free(tmpInput);
+			return result;
+		}
+		result = tmpResult;
+		result[*count]=(char*)strdup(token);
+		(*count)++;
+
+		token = strtok(NULL, ",");
+	}
+	free(tmpInput);
+	return result;
 }
